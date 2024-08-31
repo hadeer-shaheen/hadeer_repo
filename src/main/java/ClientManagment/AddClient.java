76083bf53322c8cfe555cd1f5b00443451ab165d
Hadeer Esmat <hadeeresmat68@gmail.com>
2024-08-31 09:42:12 +0300

    testngxml

--- src/main/java/ClientManagment/AddClient.java
+++ src/main/java/ClientManagment/AddClient.java
@@ -9,10 +9,10 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class AddClient
 {
     private static WebDriver driver= Utilities.getDriver();
-
+    static By AddclientButton=By.cssSelector("//i[@class='mdi mdi-account-plus d-block fs-36 mb-2 text-navy-s2020']");
     public static void AddNewCustomer(String ClientName)
     {
-        driver.findElement(By.xpath("//i[@class='mdi mdi-account-plus d-block fs-36 mb-2 text-navy-s2020']")).click();
+        driver.findElement(AddclientButton).click();
         WebElement Searchfield = driver.findElement(By.id("ClientBusinessName"));
         Searchfield.sendKeys(ClientName);
         driver.findElement(By.id("submitBtn")).click();
